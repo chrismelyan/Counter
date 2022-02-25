d98c03b788feae932add4fed6806500e7aa52ea9
chrismelyan <chris.shelpuk@gmail.com>
2022-02-25 20:17:26 +0100

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import CounterDisplay from "./CounterDisplay";
+import Buttons from "./Buttons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const maxValue = 5
+    const startValue = 0
+    const countValue = 1
+    const [counter, setCounter] = useState<number>(startValue)
+
+    const increment = () => {
+        counter < maxValue && setCounter(counter + countValue)
+    }
+    const reset = () => setCounter(startValue)
+
+    return (
+        <div className="App">
+            <div className='counter-wrapper'>
+                <CounterDisplay counter={counter} maxValue={maxValue}/>
+                <Buttons
+                    increment={increment}
+                    reset={reset}
+                    counter={counter}
+                    maxValue={maxValue}
+                    startValue={startValue}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
